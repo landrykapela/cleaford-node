82bff11bae1ca1c189efda5232f4df7e74afaa45
Landry Kapela <landrykapela@gmail.com>
2021-07-01 17:36:20 +0300

    Fix HTTPS bug in Server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const https = require("https");
 const http = require("http");
 const fs = require("fs");
-const key = fs.readFileSync("/var/certs/cert.key").toString();
-const cert = fs.readFileSync("/var/certs/cert.pem").toString();
+const key = fs.readFileSync("../certs/cert.key").toString();
+const cert = fs.readFileSync("../certs/cert.crt").toString();
 
 const app = require("./app");
 const credentials = { key: key, cert: cert };
@@ -11,11 +11,11 @@ const credentials = { key: key, cert: cert };
 const port = process.env.PORT;
 // const port = 8000;
 app.set("port", port);
-//const secure_server = https.createServer(credentials, app);
-const server = http.createServer(app);
-//secure_server.listen(s_port, () => {
-//  console.log("Listening on secure port: ", s_port);
-//});
-server.listen(port, () => {
-  console.log("cleaford running on port: ", port);
+const secure_server = https.createServer(credentials, app);
+// const server = http.createServer(app);
+secure_server.listen(port, () => {
+ console.log("Listening on secure port: ", port);
 });
+// server.listen(port, () => {
+//   console.log("cleaford running on port: ", port);
+// });
